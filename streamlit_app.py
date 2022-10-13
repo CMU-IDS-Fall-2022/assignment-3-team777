9252e1865d0681351834fdf8081e321d1ccd95f6
varusj <395291995@qq.com>
2022-10-13 14:18:43 -0400

    finish the design

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,15 @@
+from calendar import month
+from tkinter import N
 import streamlit as st
 import pandas as pd
 import altair as alt
-
+import numpy as np
 import pyreadr
 
 
-st.title("Let's analyze some Penguin Data 🐧📊.")
+st.title("Assignment 3 for IDS -- Storm tracks data analysis 🌪🌪🌪")
+
+
 
 @st.cache  # add caching so we load the data only once
 def load_data():
@@ -16,80 +20,27 @@ def load_data():
 # df = load_data()
 
 
-
 main_df = pyreadr.read_r('storms.rda')['storms']
 main_df.rename(columns={'long':'lon'}, inplace=True)
 main_df.dropna(subset=['wind','pressure', 'status'], inplace=True)
 main_df['year'] = main_df['year'].astype('int64')
 main_df['month'] = main_df['month'].astype('int64')
 
-st.write("Let's first look at raw data in the Pandas Data Frame.")
+st.subheader("Let's first take a look at raw data in the Pandas Data Frame.")
 
 st.write(main_df)
 
+st.write("The data source is from [dplyr's storm tracks data](https://dplyr.tidyverse.org/reference/storms.html#ref-examples). The meanings for each variable are provided below:")
 
-if "visibility" not in st.session_state:
-    st.session_state.visibility = "visible"
-    st.session_state.disabled = False
 
-col1, col2 = st.columns(2)
+st.markdown("- name: Storm Name \n - year,month,day: Date of report \n - hour: Hour of report (in UTC) \n - lat,long: Location of storm center \n - status: Storm classification (Tropical Depression, Tropical Storm, or Hurricane) \n - category: Saffir-Simpson storm category (estimated from wind speed. -1 = Tropical Depression, 0 = Tropical Storm)\n - wind: storm's maximum sustained wind speed (in knots)\n  - pressure:Air pressure at the storm\'s center (in millibars)\n - tropicalstorm_force_diameter: Diameter (in nautical miles) of the area experiencing tropical storm strength winds (34 knots or above)\n - hurricane_force_diameter: Diameter (in nautical miles) of the area experiencing hurricane strength winds (64 knots or above)\n There are something missing in the raw data. So we will process the data according to our proposed question before visualization.")
 
-with col1:
-    st.checkbox("Disable selectbox widget", key="disabled")
-    st.radio(
-        "Set selectbox label visibility 👉",
-        key="visibility",
-        options=["visible", "hidden", "collapsed"],
-    )
 
-with col2:
-    option = st.selectbox(
-        "How would you like to be contacted?",
-        ("Email", "Home phone", "Mobile phone"),
-        label_visibility=st.session_state.visibility,
-        disabled=st.session_state.disabled,
-    )
+st.subheader("Then, let's look at the statistics of this dataset:")
 
 
 
 
-st.map(main_df)
-
-st.write('Let\'s choose a few storms to visualize.')
-st.write('By Name:')
-
-names = st.multiselect('Name', main_df['name'].unique())
-chosen_name_df = main_df[main_df['name'].isin(names)]
-st.map(chosen_name_df)
-
-st.write('By Time:')
-years = st.multiselect('Year', main_df['year'].unique())
-months = st.multiselect('Month', main_df['month'].unique(), default=main_df['month'].unique())
-if years is not None:
-    chosen_year_df = main_df[main_df['year'].isin(years)]
-if months is not None:
-    chosen_year_df = chosen_year_df[chosen_year_df['month'].isin(months)]
-st.map(chosen_year_df)
-
-
-
-st.write("Hmm 🤔, is there some correlation between body mass and flipper length? Let's make a scatterplot with [Altair](https://altair-viz.github.io/) to find.")
-
-# chart = alt.Chart(df).mark_point().encode(
-#     x=alt.X("body_mass_g", scale=alt.Scale(zero=False)),
-#     y=alt.Y("flipper_length_mm", scale=alt.Scale(zero=False)),
-#     color=alt.Y("species")
-# ).properties(
-#     width=600, height=400
-# ).interactive()
-
-# st.write(chart)
-
-
-"""
-Data process: Group the dataframe by storm names, 
-and calculate the duration of each storm, getting rid of the ones too long.
-"""
 storm_duration = pd.DataFrame(columns=['duration', 'wind', 'pressure', 'status'])
 dfs = []
 
@@ -108,10 +59,7 @@ for name, group in main_df.groupby('name'):
 
 storm_duration = pd.concat(dfs)
 
-"""
-Visualization of interactive exploration chart, analysing relationship 
-among duration, the type of storms, pressure, and max wind speed.
-"""
+
 
 scale = alt.Scale(domain=storm_duration['status'].value_counts().keys().to_list())
 color = alt.Color('status:N', scale=scale, title='Storm Category')
@@ -159,6 +107,21 @@ chart1 = alt.vconcat(
 
 st.write(chart1)
 
+"""
+Graph 1:\n
+\n\n
+- Data process: Group the dataframe by storm names, and calculate the duration of each storm, getting rid of the ones too long.
+
+- Purpose: Visualization of interactive exploration chart, analysing relationship among duration, the type of storms, pressure, and max wind speed.
+"""
+
+
+st.markdown("**Interactive hint**: \nYou can select an area of interest in the above scatter plot, the corresponding data statistics will be updated simultanueously in the bar plot below. You can also click the bars in the bar plot to check the statistics of a particualr storm type.")
+
+
+
+
+
 
 drop_df = main_df.dropna(subset=['tropicalstorm_force_diameter'])
 
@@ -203,6 +166,96 @@ chart2=alt.vconcat(
     size="independent"
 )
 st.write(chart2)
+
+
+"""
+Graph 2:\n
+\n\n
+- Data process: Drop the rows with a 0 or NaN in the column tropicalstorm_force_diameter.
+- Purpose: Visualization of the relation between the frequency of wind speed and pressure and the diameter of tropical storms, and we add a function of selecting the tpye of storms to provide special perspectives.
+"""
+
+st.markdown("**Interactive hint**: \n You can click the bars in the bar plot to check the statistics of a particualr storm type.")
+
+
+# chart3=alt.Chart(main_df[2000:7000]).mark_boxplot(extent='min-max').encode(
+#     x='month:Q',
+#     y= 'wind:Q',
+# #     color='pressure:N',
+# #     column='pressure:N'
+# ).properties(
+#     width = 500
+# )
+# st.write(chart3)
+
+
+st.subheader("Next, let's look at the traces of these storms over the years")
+
+st.map(main_df)
+
+st.write("It's a bit of messy, right 🙂? No, actually it is extremely messy and of disorder, looking like chaos 🤯🤯🤯 !! ")
+
+
+st.write('But, if we can pick only a few traces according to certain features 🤩, we will have:')
+
+st.write('Selected by Name:')
+
+names = st.multiselect('Name', main_df['name'].unique())
+chosen_name_df = main_df[main_df['name'].isin(names)]
+st.map(chosen_name_df)
+
+if len(names) == 0:
+    st.write("Please choose the year and month you wish to see")
+
+st.write('Selected by Year and Month:')
+
+col1, col2 = st.columns(2)
+
+with col1:
+    years = st.multiselect('Year', main_df['year'].unique())
+
+with col2:
+    months = st.multiselect('Month', np.sort(main_df['month'].unique()))
+
+
+if len(years) == 0:
+    st.write("Please choose the year and month you wish to see")
+else:
+    chosen_year_df = main_df[main_df['year'].isin(years)]
+
+if len(months) == 0:
+    chosen_year_df = main_df[main_df['year'].isin(years)]
+else:
+    chosen_year_df = chosen_year_df[chosen_year_df['month'].isin(months)]
+
+if len(years) > 0 and len(months) > 0 and len(chosen_year_df) == 0:
+    st.write("No record!")
+
+st.map(chosen_year_df)
+
+
+
+
+# chart = alt.Chart(df).mark_point().encode(
+#     x=alt.X("body_mass_g", scale=alt.Scale(zero=False)),
+#     y=alt.Y("flipper_length_mm", scale=alt.Scale(zero=False)),
+#     color=alt.Y("species")
+# ).properties(
+#     width=600, height=400
+# ).interactive()
+
+# st.write(chart)
+
+"""
+Now, you can clearly see certain tracks of storms by the name or time you select!! Thank you! \n
+
+References:
+- https://dplyr.tidyverse.org/reference/storms.html
+- https://altair-viz.github.io/index.html
+- https://docs.streamlit.io/
+
+"""
+
 
 
 
